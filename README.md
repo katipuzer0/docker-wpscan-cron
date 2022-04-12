3412c2f0a81cb4ecaa068efcbb41c6469fee6117
katipuzer0 <katipuzero@gmail.com>
2022-04-12 11:06:10 -0400

    fix: make slack channel as env variable

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 ### Introduction
 
 Cron-schedule WPScan of Wordpress sites using docker and docker-compose with results 
-being posted to a slack channel.
-
+summary posted to a slack channel.
 
 ### Usage
 
-* Edit crontab.txt to reflect the desired schedule to run the scan
+First obtain a WPScan API token then create a new app in your Slack workspace.
+
+
+* Edit `crontab.txt` to reflect the desired schedule to run the scan
 * Create a `.env` file
 * Set the `SLACK_OAUTH_TOKEN` value in the `.env`
+* Set the `SLACK_CHANNEL` value in the `.env`
 * Set the `WPSCAN_API_TOKEN` value in the `.env`
-* Add the sites to scan in wp-urls.txt
+* Add the Wordpress sites to scan in `/shared/wp-urls.txt`
 
 `docker-compose build`
 
--- shared/start.sh
+++ shared/start.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-#Needed environment variables
-# $SLACK_WEBHOOK_URL
-# $WPSCAN_API_TOKEN
+
+#perform some checks
+if [[ -z "$WPSCAN_API_TOKEN" ]]; then
+    echo "Must set WPSCAN_API_TOKEN" 1>&2
+    exit 1
+fi
+
+if [[ -z "$SLACK_AUTH_TOKEN" ]]; then
+    echo "Must set SLACK_AUTH_TOKEN" 1>&2
+    exit 2
+fi
+
+if [[ -z "$SLACK_CHANNEL" ]]; then
+    echo "Must set SLACK_CHANNEL" 1>&2
+    exit 3
+fi
 
 
 wpfile="/shared/wp-urls.txt" 
@@ -14,5 +27,5 @@ while read p; do
   RESULT_FILENAME=/shared/archive/$TARGET.wpscan.`date +"%Y-%m-%d"`
   wpscan --url $TARGET --detection-mode aggressive --random-user-agent --ignore-main-redirect --force --no-banner  --api-token $WPSCAN_API_TOKEN --wp-content-dir wp-content --enumerate u,vp,vt,tt,cb,dbe -f cli-no-color --output $RESULT_FILENAME.json --format json
   python3 -m wpscan_out_parse --no_color $RESULT_FILENAME.json > $RESULT_FILENAME.txt 
-  curl -F file=@${RESULT_FILENAME}.txt -F filetype=text -F "initial_comment=Today's WPScan Results" -F channels=uplb-website -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
+  curl -F file=@${RESULT_FILENAME}.txt -F filetype=text -F "initial_comment=Today's WPScan Results" -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
 done <$wpfile
