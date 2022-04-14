68cda39514ea5f0b75d1c0f5057efae8f38bc9a8
katipuzer0 <katipuzero@gmail.com>
2022-04-14 11:47:07 -0400

    fix: add --update in call to wpscan

--- shared/start.sh
+++ shared/start.sh
@@ -25,7 +25,7 @@ while read p; do
   TARGET=`echo $p | sed -e 's#.*//##' | sed -e 's#/.*##' `
   echo $TARGET
   RESULT_FILENAME=/shared/archive/$TARGET.wpscan.`date +"%Y-%m-%d"`
-  wpscan --url $TARGET --detection-mode aggressive --random-user-agent --ignore-main-redirect --force --no-banner  --api-token $WPSCAN_API_TOKEN --wp-content-dir wp-content --enumerate u,vp,vt,tt,cb,dbe -f cli-no-color --output $RESULT_FILENAME.json --format json
+  wpscan --update --url $TARGET --detection-mode aggressive --random-user-agent --ignore-main-redirect --force --no-banner  --api-token $WPSCAN_API_TOKEN --wp-content-dir wp-content --enumerate u,vp,vt,tt,cb,dbe -f cli-no-color --output $RESULT_FILENAME.json --format json
   python3 -m wpscan_out_parse --no_color $RESULT_FILENAME.json > $RESULT_FILENAME.txt 
   curl -F file=@${RESULT_FILENAME}.txt -F filetype=text -F "initial_comment=Today's WPScan Results" -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
 done <$wpfile
