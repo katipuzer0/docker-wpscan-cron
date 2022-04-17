4b2b6190a08e4bf97a4b791d4a928c0971727642
katipuzer0 <katipuzero@gmail.com>
2022-04-16 23:54:56 -0400

    fix: errors in start.sh where corrected

--- shared/start.sh
+++ shared/start.sh
@@ -7,8 +7,8 @@ if [[ -z "$WPSCAN_API_TOKEN" ]]; then
     exit 1
 fi
 
-if [[ -z "$SLACK_AUTH_TOKEN" ]]; then
-    echo "Must set SLACK_AUTH_TOKEN" 1>&2
+if [[ -z "$SLACK_OAUTH_TOKEN" ]]; then
+    echo "Must set SLACK_OAUTH_TOKEN" 1>&2
     exit 2
 fi
 
@@ -20,12 +20,13 @@ fi
 
 wpfile="/shared/wp-urls.txt" 
 
+wpscan --update
 while read p; do 
   echo $p
   TARGET=`echo $p | sed -e 's#.*//##' | sed -e 's#/.*##' `
   echo $TARGET
-  RESULT_FILENAME=/shared/archive/$TARGET.wpscan.`date +"%Y-%m-%d"`
-  wpscan --update --url $TARGET --detection-mode aggressive --random-user-agent --ignore-main-redirect --force --no-banner  --api-token $WPSCAN_API_TOKEN --wp-content-dir wp-content --enumerate u,vp,vt,tt,cb,dbe -f cli-no-color --output $RESULT_FILENAME.json --format json
+  RESULT_FILENAME=/shared/archive/wpscan.$TARGET.`date +"%Y-%m-%d"`
+  wpscan --disable-tls-checks --url $TARGET --detection-mode aggressive --random-user-agent --ignore-main-redirect --force --no-banner  --api-token $WPSCAN_API_TOKEN --wp-content-dir wp-content --enumerate u,vp,vt,tt,cb,dbe -f cli-no-color --output $RESULT_FILENAME.json --format json
   python3 -m wpscan_out_parse --no_color $RESULT_FILENAME.json > $RESULT_FILENAME.txt 
   curl -F file=@${RESULT_FILENAME}.txt -F filetype=text -F "initial_comment=Today's WPScan Results" -F channels=$SLACK_CHANNEL -H "Authorization: Bearer $SLACK_OAUTH_TOKEN" https://slack.com/api/files.upload
 done <$wpfile
